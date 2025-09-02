ddd39bf2f765b77597c13c65a4ea3c2284bd7a66
dennisleister <dennleist@gmail.com>
2025-09-02 22:03:58 +0200

    remove test directory from .gitignore

---
+++ backend/src/test/java/de/dele1907/Controller/ArcherControllerTest.java
@@ -0,0 +1,62 @@
+package de.dele1907.Controller;
+
+import de.dele1907.Model.Archer;
+import de.dele1907.Service.ArcherService;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArcherControllerTest {
+
+    @Test
+    void testGetAllArchersEndpoint() {
+        ArcherService mockService = new ArcherService() {
+            @Override
+            public List<Archer> getAllArchers() {
+                return List.of(
+                        new Archer("4711","Doe", "John", "TestClub", 42, "Recurve", "Adult"),
+                        new Archer("0815","Smith", "Jane", "AnotherClub", 99, "Compound", "Junior")
+                );
+            }
+        };
+
+        Javalin app = Javalin.create();
+        new ArcherController(mockService).registerRoutes(app);
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/archers");
+            assertEquals(200, response.code());
+            String json = response.body().string();
+
+            assertTrue(json.contains("Doe"));
+            assertTrue(json.contains("Smith"));
+        });
+    }
+
+    @Test
+    void getArcherById() {
+        ArcherService mockService = new ArcherService() {
+            @Override
+            public List<Archer> getAllArchers() {
+                return List.of(
+                        new Archer("4711","Doe", "John", "TestClub", 42, "Recurve", "Adult"),
+                        new Archer("0815","Smith", "Jane", "AnotherClub", 99, "Compound", "Junior")
+                );
+            }
+        };
+        String id = "4711";
+
+        Javalin app = Javalin.create();
+        new ArcherController(mockService).registerRoutes(app);
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/archers/" + id);
+            assertEquals(200, response.code());
+            assertTrue(response.body().string().contains("Napp"));
+        });
+    }
+}
