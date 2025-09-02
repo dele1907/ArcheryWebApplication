7c8a553c3b81a37d559f56424301d63f27a4c537
dennisleister <dennleist@gmail.com>
2025-09-02 22:17:01 +0200

    refactor App and DashboardPage components for improved code style and consistency

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,18 @@
 <script setup lang="ts">
-
-import DashboardPage, {type Archer} from "@/components/pages/DashboardPage.vue";
-import "./style/style.css"
+import DashboardPage, { type Archer } from '@/components/pages/DashboardPage.vue'
+import './style/style.css'
 
 const URL = 'http://localhost:7070/archers'
 
 const getAllArchers = async (): Promise<Array<Archer>> => {
-  const response = await fetch(URL);
+  const response = await fetch(URL)
 
-  return await response.json();
+  return await response.json()
 }
-
-
 </script>
 
 <template>
   <DashboardPage :onGetAllArchers="getAllArchers" />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- frontend/src/components/pages/DashboardPage.vue
+++ frontend/src/components/pages/DashboardPage.vue
@@ -1,42 +1,42 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import HeaderBar from "@/components/HeaderBar.vue";
+import { ref } from 'vue'
+import HeaderBar from '@/components/HeaderBar.vue'
 
 export type Archer = {
-  "name": string,
-  "firstName": string,
-  "club": string,
-  "clubNumber": number,
-  "id": string,
-  "bowType": string,
-  "ageCategory": string
+  name: string
+  firstName: string
+  club: string
+  clubNumber: number
+  id: string
+  bowType: string
+  ageCategory: string
 }
 
-const props = defineProps<{onGetAllArchers: () => Promise<Array<Archer>>}>();
+const props = defineProps<{ onGetAllArchers: () => Promise<Array<Archer>> }>()
 
-const archers = ref<Array<Archer>>([]);
+const archers = ref<Array<Archer>>([])
 
 const onGetAllArchers = async () => {
-  archers.value = await props.onGetAllArchers();
-};
+  archers.value = await props.onGetAllArchers()
+}
 </script>
 
 <template>
   <div data-testid="dashboard-page" class="page">
     <div class="page-header">
-        <HeaderBar/>
+      <HeaderBar />
     </div>
 
     <p>Welcome to the Archery Tournament Dashboard!</p>
 
-    <button @click=onGetAllArchers >Get All Archers</button>
+    <button @click="onGetAllArchers">Get All Archers</button>
 
     <div class="archers-list">
       <ul v-if="archers.length > 0">
         <li v-for="archer in archers" :key="archer.id">
           <div class="archer-wrapper">
             {{ archer.name }}
-            {{archer.firstName}}
+            {{ archer.firstName }}
             {{ archer.club }}
           </div>
         </li>
