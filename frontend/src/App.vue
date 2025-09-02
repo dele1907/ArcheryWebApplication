1e34a35c271a65885cdc27538910f6fb859e4cfc
dennisleister <dennleist@gmail.com>
2025-09-02 22:00:07 +0200

    add DashboardPage component and implement archers fetching logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import DashboardPage, {type Archer} from "@/components/DashboardPage.vue";
+
+const URL = 'http://localhost:7070/archers'
+
+const getAllArchers = async (): Promise<Array<Archer>> => {
+  const response = await fetch(URL);
+
+  return await response.json();
+}
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <DashboardPage :onGetAllArchers="getAllArchers" />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
---
+++ frontend/src/components/DashboardPage.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+export type Archer = {
+  "name": string,
+  "firstName": string,
+  "club": string,
+  "clubNumber": number,
+  "id": string,
+  "bowType": string,
+  "ageCategory": string
+}
+
+const props = defineProps<{onGetAllArchers: () => Promise<Array<Archer>>}>();
+
+const archers = ref<Array<Archer>>([]);
+
+const onGetAllArchers = async () => {
+  archers.value = await props.onGetAllArchers();
+};
+</script>
+
+<template>
+  <div data-testid="dashboard-page" class="dashboard-page">
+    <h1>Dashboard</h1>
+    <p>Welcome to the Archery Tournament Dashboard!</p>
+    <button @click=onGetAllArchers >Get All Archers</button>
+
+    <div class="archers-list">
+      <ul v-if="archers.length > 0">
+        <li v-for="archer in archers" :key="archer.id">
+          <div class="archer-wrapper">
+            {{ archer.name }}
+            {{archer.firstName}}
+            {{ archer.club }}
+          </div>
+        </li>
+      </ul>
+
+      <p v-else>No archers to display yet.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.archer-wrapper {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  background-color: #f9f9f9;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
