0c13daca3c57f46015d9f185dc5a9de329b503d8
dennisleister <dennleist@gmail.com>
2025-09-05 21:34:17 +0200

    add DashboardCard component and integrate it into DashboardPage; remove unused code

---
+++ frontend/src/components/dashboardcomponents/DashboardCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import router from "@/router";
+
+const props = defineProps<{
+  title: string
+  imageSrc: string
+  link: string
+}>()
+
+const onCardClicked = () => {
+  const resolvedRoute = router.resolve(props.link);
+  if (resolvedRoute.matched.length > 0) {
+    router.push(props.link);
+  } else {
+    alert(`Route ${props.link} existiert nicht!`);
+  }
+}
+</script>
+
+<template>
+  <v-card @click="onCardClicked" width="500" height="300" class="ma-4">
+    <v-img :src="imageSrc"></v-img>
+    <v-card-title class="text-center">{{title}}</v-card-title>
+  </v-card>
+</template>
+
+<style scoped>
+.v-card {
+  cursor: pointer;
+  justify-content: center;
+  align-content: center;
+
+  &:hover {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    transform: translateY(-2px);
+    transition: all 0.3s ease;
+  }
+}
+</style>
--- frontend/src/components/pages/DashboardPage.vue
+++ frontend/src/components/pages/DashboardPage.vue
@@ -1,61 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-export type Archer = {
-  name: string
-  firstName: string
-  club: string
-  clubNumber: number
-  id: string
-  bowType: string
-  ageCategory: string
-}
-const archers = ref<Array<Archer>>([])
-const URL = 'http://localhost:7070/archers'
-
-const onGetAllArchers = async (): Promise<void> => {
-  const response = await fetch(URL)
-
-  archers.value = await response.json()
-}
+import DashboardCard from "@/components/dashboardcomponents/DashboardCard.vue";
+import dashboard_archer_overview from '@/assets/images/dashboard/dashboard_archer_overview_image.png';
 </script>
 
 <template>
   <div data-testid="dashboard-page" class="page">
-    <p>Welcome to the Archery Tournament Dashboard!</p>
-    <button @click="onGetAllArchers">Get All Archers</button>
-
-    <div class="archers-list">
-      <ul v-if="archers.length > 0">
-        <li v-for="archer in archers" :key="archer.id">
-          <div class="archer-wrapper">
-            {{ archer.name }} |
-            {{ archer.firstName }} |
-            {{ archer.club }} |
-            {{ archer.bowType }} |
-            {{ archer.ageCategory }}
-          </div>
-        </li>
-      </ul>
-
-      <p v-else>No archers to display yet.</p>
+    <div class="dashboard-menu-item-wrapper">
+      <DashboardCard title="Schützenverwaltung" :image-src="dashboard_archer_overview" link="/archers"/>
+      <DashboardCard title="Ergebnisübersicht" image-src="" link="/results"/>
     </div>
   </div>
 </template>
 
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.archer-wrapper {
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin-bottom: 10px;
-  border-radius: 10px;
-  background-color: #f9f9f9;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  text-align: center;
+.dashboard-menu-item-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 </style>
