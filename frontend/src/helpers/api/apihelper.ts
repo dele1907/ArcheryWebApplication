412caf7b4478a538bb3673d1c83fc0ed9c0467e8
dennisleister <dennleist@gmail.com>
2025-09-09 18:12:47 +0200

    remove unused Archer methods from ApiHelper; clean up code and improve maintainability

--- frontend/src/helpers/api/apihelper.ts
+++ frontend/src/helpers/api/apihelper.ts
@@ -1,5 +1,3 @@
-import type { Archer } from '../../../types/types.ts';
-
 export class ApiHelper {
   private static API_URL = 'http://localhost:7070/';
 
@@ -34,26 +32,6 @@ export class ApiHelper {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
-    }).then((response) => response);
-  }
-
-  public static async fetchShooters(): Promise<Array<Archer>> {
-    return fetch(this.API_URL + 'archers').then((response) => response.json());
-  }
-
-  public static async createShooter(shooter: Archer): Promise<Response> {
-    return fetch(this.API_URL + 'archers', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(shooter),
-    }).then((response) => response);
-  }
-
-  public static async deleteShooter(id: string): Promise<Response> {
-    return fetch(this.API_URL + 'archers/' + id, {
-      method: 'DELETE',
     }).then((response) => response);
   }
 }
